9f9aeb98e9955f06db16df09a9591aba849f0957
Fabio <fabio.marmello@gmail.com>
2025-05-29 15:24:40 +0200

    from 100 to 1000 samples

--- code_classification/tmp.py
+++ code_classification/tmp.py
@@ -3,5 +3,6 @@ import pandas as pd
 with open("./data/test_dataset.jsonl", "r") as f:
     df = pd.read_json(f, lines=True)
     df = df.rename(columns={"code": "text", "language": "language"})
-    df.sample(100).to_json("./data/random_concept_dataset.jsonl", orient="records", lines=True)
+    df.sample(1000).to_json("./data/random_concept_dataset.jsonl", orient="records", lines=True)
+    
     
